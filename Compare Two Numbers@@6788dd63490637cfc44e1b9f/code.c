f68af265d3bcedf20cdcb00131882215907270da
twinkle362 <twinkle1849.becse24@chitkara.edu.in>
2025-02-11 00:43:05 +0530

    Solution to Compare Two Numbers

---
+++ Compare Two Numbers@@6788dd63490637cfc44e1b9f/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int x,y;
+    scanf("%d",&x,&y);
+    if(x>y){
+        printf("First");
+    }
+    else if(y>x){
+        printf("Second");
+    }
+    else{"Equal"}
+}
