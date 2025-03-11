89aeeb41b89d4dfe9da2e7f320a926fe5449a81f
twinkle362 <twinkle1849.becse24@chitkara.edu.in>
2025-03-11 17:02:48 +0530

    Solution to Count Students Scoring Above a Threshold

--- Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
+++ Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
@@ -1,34 +1,65 @@
-// Your code here...
-#include <stdio.h>
+// // Your code here...
+// #include <stdio.h>
 
-struct Stu {
+// struct Stu {
+//     int RoNum;
+//     char name[50];
+//     float marks;
+// };
+
+// int main() {
+//     int n;
+//     float check;
+//     int k=0;
+//     scanf("%d", &n);
+    
+//     float a = 0;
+//     struct Stu Sts[n]; 
+    
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &Sts[i].RoNum);
+//         scanf("%s", &Sts[i].name); 
+//         scanf("%f", &Sts[i].marks);
+//     }
+//     scanf("%f",&check);
+
+//     for (int i = 0; i < n; i++) {
+//         check<= Sts[i].marks;
+//         k++;
+//     }
+
+//     printf("Count of students scoring above %.2f: %d", check,k);
+
+//     return 0;
+// }
+#include <stdio.h>
+#include <string.h>
+
+struct Stu{
     int RoNum;
     char name[50];
     float marks;
 };
 
-int main() {
+int main(){
+    struct Stu Sts[100];
     int n;
-    float check;
-    int k=0;
     scanf("%d", &n);
-    
-    float a = 0;
-    struct Stu Sts[n]; 
-    
-    for (int i = 0; i < n; i++) {
+    float m;
+    for (int i = 0; i < n; i++){
         scanf("%d", &Sts[i].RoNum);
-        scanf("%s", &Sts[i].name); 
+        scanf("%s", Sts[i].name);
         scanf("%f", &Sts[i].marks);
     }
-    scanf("%f",&check);
+    scanf("%f", &m);
+    printf("%.2f\n", m);
 
-    for (int i = 0; i < n; i++) {
-        check<= Sts[i].marks;
-        k++;
+    int k = 0;
+    for (int i = 0; i < n; i++){
+        if (Sts[i].marks > m){
+            k++;
+        }
     }
-
-    printf("Count of students scoring above %.2f: %d", check,k);
-
+    printf("Count of students scoring above %.2f: %d\n", m, k);
     return 0;
 }
