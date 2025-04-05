f77ea639f9b7689b952b484f9e8fcea2feff2bf4
twinkle362 <twinkle1849.becse24@chitkara.edu.in>
2025-04-05 11:54:53 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -30,27 +30,56 @@
 //     }
     
 // }
-
-
-#include<stdio.h>
-int main(){
-    int n;
-    int arr2[1000]={0};
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     int arr2[1000]={0};
     
-    scanf("%d",&n);
+//     scanf("%d",&n);
+//     int arr[n];
+//     int printed[1000]={0};
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i=0;i<1000;i++){
+//         arr2[arr[i]]+=1;
+//     }
+//     for(int i=0;i<n;i++){
+//        if (!printed[arr[i]]) {
+//             printf("%d %d\n", arr[i], arr2[arr[i]]);
+//             printed[arr[i]] = 1;
+//         }
+//     }
+// return 0;
+// }
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr2[1000] = {0};
+    int printed[1000] = {0};
+
+    scanf("%d", &n);
     int arr[n];
-    int printed[1000]={0};
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        // Safety check for range
+        if (arr[i] < 0 || arr[i] >= 1000) {
+            printf("Error: input value %d is out of allowed range (0-999)\n", arr[i]);
+            return 1; // Exit program due to invalid input
+        }
+
+        arr2[arr[i]] += 1;
     }
-    for(int i=0;i<1000;i++){
-        arr2[arr[i]]+=1;
-    }
-    for(int i=0;i<n;i++){
-       if (!printed[arr[i]]) {
+
+    for (int i = 0; i < n; i++) {
+        if (!printed[arr[i]]) {
             printf("%d %d\n", arr[i], arr2[arr[i]]);
             printed[arr[i]] = 1;
+        }
     }
-}
-return 0;
+
+    return 0;
 }
