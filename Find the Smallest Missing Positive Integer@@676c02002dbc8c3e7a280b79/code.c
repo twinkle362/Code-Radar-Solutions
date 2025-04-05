cc1df6e94f3873a57c3b90a23825345e50cb5c4a
twinkle362 <twinkle1849.becse24@chitkara.edu.in>
2025-04-05 15:27:35 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -25,10 +25,47 @@
 //     printf("%d\n", missing);
 //     return 0;
 // }
+#include<stdio.h>
+void bubbleSort(int arr[], int n) {
+
+    // Bubble sort
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    if(n==1){
+        printf("%d",1);
+        return;
+    }
+    for(int i=1;i<n;i++){
+        if(arr[i-1]!=arr[i]-1){
+            printf("%d\n",(arr[i-1]+1));
+            break;
+        }
+
+        else {
+             printf("%d\n",(arr[n-1]+1));
+             break;
+        }
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    bubbleSort(arr, n);
+}
 // #include<stdio.h>
 // void bubbleSort(int arr[], int n) {
 
-//     // Bubble sort
 //     for (int i = 0; i < n - 1; i++) {
 //         for (int j = 0; j < n - i - 1; j++) {
 //             if (arr[j] > arr[j + 1]) {
@@ -38,44 +75,24 @@
 //             }
 //         }
 //     }
-//     if(n==1){
-//         printf("%d",1);
-//         return;
-//     }
-//     for(int i=1;i<n;i++){
-//         if(arr[i-1]!=arr[i]-1){
-//             printf("%d\n",(arr[i-1]+1));
-//             break;
-//         }
-
-//         else {
-//              printf("%d\n",(arr[n-1]+1));
-//              break;
-//         }
-//     }
 // }
 // int main(){
 //     int n;
 //     scanf("%d",&n);
 //     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
+
+//     bubbleSort(arr,n);
+
+//     for(int i=0;i<n;){
+//         if(arr[i]>0){
+//             if(arr[i]==1){
+//                for(int j=i;j<n;j++){
+//                 if(arr[j+1]!=arr[j]+1){
+
+//                 }
+//                }
+//             }
+//         }
 //     }
-//     bubbleSort(arr, n);
+   
 // }
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    int k=1;
-    for(int i=0;i<n;i++){
-        if(arr[i]==k){
-            k++;
-            continue;
-        }
-        else{
-            printf("%d",1);
-        }
-    }
-}
