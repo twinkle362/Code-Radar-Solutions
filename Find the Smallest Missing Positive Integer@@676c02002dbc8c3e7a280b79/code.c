7aa8513ca8230f58217bb456131fcc8c1decb0c8
Twinkle Jindal <twinklejindal001@gmail.com>
2025-04-05 18:03:16 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -1,35 +1,5 @@
-// // Your code here...
-// #include <stdio.h>
-
-// int main() {
-//     int n, arr[100], i, j, missing = 1;
-//     scanf("%d", &n);
-
-//     for (i = 0; i < n; i++) {
-//         scanf("%d", &arr[i]);
-//     }
-//     for (i = 1; ; i++) { 
-//         int found = 0;
-//         for (j = 0; j < n; j++) {
-//             if (arr[j] == i) {
-//                 found = 1;
-//                 break;
-//             }
-//         }
-//         if (!found) {
-//             missing = i;
-//             break;
-//         }
-//     }
-
-//     printf("%d\n", missing);
-//     return 0;
-// }
 #include<stdio.h>
 void bubbleSort(int arr[], int n) {
-    int ans;
-
-    // Bubble sort
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -39,33 +9,32 @@ void bubbleSort(int arr[], int n) {
             }
         }
     }
-    if(n==1){
-        printf("%d",1);
-        return;
-    }
-    for(int i=1;i<n;i++){
-        if(arr[i-1]!=arr[i]-1){
-            ans=arr[i-1]+1;
-            break;
-        }
-
-        else {
-            ans=arr[n-1]+1;
-            break;
-        }
-    }
-    if (ans<=0){
-        printf("%d",1);
-    }
 }
 int main(){
     int n;
+    int start;
     scanf("%d",&n);
     int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
     bubbleSort(arr, n);
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+            start=i;
+            break;
+        }
+        else{
+            printf("%d",1);
+        }
+    }
+    for(int j=start+1;j<n;j++){
+        if(arr[j-1]!=arr[j]){
+            printf("%d",arr[j-1]+1);
+            break;
+        }
+    }
+
 }
 // #include<stdio.h>
 // void bubbleSort(int arr[], int n) {
