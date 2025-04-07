47ec7bfde6bdc596a6c8379f7df191a7fbe11a0b
Twinkle Jindal <twinklejindal001@gmail.com>
2025-04-07 18:51:18 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -1,6 +1,44 @@
-// Your code here...
+// // Your code here...
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int max=0;
+//     for(int i=0;i<n;i++){
+//         for(int j=i+1;j<n;j++){
+//         if(arr[i]*arr[j]>max){
+//             max=arr[i]*arr[j];
+//         }
+//         }
+//     }
+//     printf("%d",abs(max));
+    
+    
+   
+// }
 #include<stdio.h>
-#include<stdlib.h>
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    // Traverse through all array elements
+    for (i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (j = 0; j < n - i - 1; j++) {
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("%d",arr[n-2]*arr[n-1]);
+}
 int main(){
     int n;
     scanf("%d",&n);
@@ -8,16 +46,5 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    int max=0;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-        if(arr[i]*arr[j]>max){
-            max=arr[i]*arr[j];
-        }
-        }
-    }
-    printf("%d",abs(max));
-    
-    
-   
+    bubbleSort(arr,n);
 }
