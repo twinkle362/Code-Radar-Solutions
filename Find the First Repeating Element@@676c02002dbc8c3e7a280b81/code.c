64dbfb0daa2c9020c92e05d5b87cdf1049c8dd01
Twinkle Jindal <twinklejindal001@gmail.com>
2025-04-07 19:24:32 +0530

    Solution to Find the First Repeating Element

--- Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
+++ Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
@@ -1,4 +1,54 @@
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int count=1;
+//     int max=0;
+//     int max_count=1;
+//     for(int i=0;i<n;i++){
+//         count=1;
+//        for(int j=i+1;j<n;j++){
+//        if(arr[i]==arr[j]){
+//         count=count+1;
+//        }
+//     }if(count>max_count){
+//         max_count=count;
+//         max=arr[i];
+//     }
+
+//     }if(max_count==1){
+//         printf("-1");
+//     }else{
+//     printf("%d",max);}
+
+//     return 0;
+// }
 #include<stdio.h>
+void bubbleSort(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n - 1; i++) {
+        // Loop for each element
+        for (int j = 0; j < n - i - 1; j++) {
+            // Swap if current element is greater than next
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for(int i=1;i<n;i++){
+        if(arr[i-1]==arr[i]){
+            printf("%d",arr[i-1]);
+            break;
+        }
+    }
+
+}
 int main(){
     int n;
     scanf("%d",&n);
@@ -6,24 +56,5 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    int count=1;
-    int max=0;
-    int max_count=1;
-    for(int i=0;i<n;i++){
-        count=1;
-       for(int j=i+1;j<n;j++){
-       if(arr[i]==arr[j]){
-        count=count+1;
-       }
-    }if(count>max_count){
-        max_count=count;
-        max=arr[i];
-    }
-
-    }if(max_count==1){
-        printf("-1");
-    }else{
-    printf("%d",max);}
-
-    return 0;
+    bubbleSort(arr, n)
 }
