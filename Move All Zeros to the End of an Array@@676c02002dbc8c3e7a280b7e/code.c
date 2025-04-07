6b769a76f78cd50e05d7661ba0e939f9041529e7
Twinkle Jindal <twinklejindal001@gmail.com>
2025-04-07 18:25:05 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -1,5 +1,41 @@
-// Your code here...
+// // Your code here...
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int index=0;
+//     for(int i=0;i<n;i++){
+//         if (arr[i]!=0){
+//            arr[index]=arr[i];
+//            index++;
+//         }
+//     }
+//     while(index<n){
+//         arr[index]=0;
+//         index++;
+//     }
+//     for(int i=0;i<n;i++){
+//        printf("%d ",arr[i]);
+//     }
+// }
 #include<stdio.h>
+void swipe(int n,int arr[n]){
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            int tem;
+            tem=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=tem;
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+}
 int main(){
     int n;
     scanf("%d",&n);
@@ -7,18 +43,5 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    int index=0;
-    for(int i=0;i<n;i++){
-        if (arr[i]!=0){
-           arr[index]=arr[i];
-           index++;
-        }
-    }
-    while(index<n){
-        arr[index]=0;
-        index++;
-    }
-    for(int i=0;i<n;i++){
-       printf("%d ",arr[i]);
-    }
+
 }
