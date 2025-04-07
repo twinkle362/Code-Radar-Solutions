71ad58a094aebde7f579d2379961e40faec30f32
Twinkle Jindal <twinklejindal001@gmail.com>
2025-04-07 19:20:14 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -21,31 +21,53 @@
     
    
 // }
+// #include<stdio.h>
+// #include <stdlib.h> 
+// void bubbleSort(int arr[], int n) {
+//     int i, j, temp;
+//     // Traverse through all array elements
+//     for (i = 0; i < n - 1; i++) {
+//         // Last i elements are already in place
+//         for (j = 0; j < n - i - 1; j++) {
+//             // Swap if the element found is greater than the next element
+//             if (abs(arr[j]) > abs(arr[j + 1])) {
+//                 // Swap arr[j] and arr[j+1]
+//                 temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+//     printf("%d",arr[n-2]*arr[n-1]);
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     bubbleSort(arr,n);
+// }
 #include<stdio.h>
-#include <stdlib.h> 
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    // Traverse through all array elements
-    for (i = 0; i < n - 1; i++) {
-        // Last i elements are already in place
-        for (j = 0; j < n - i - 1; j++) {
-            // Swap if the element found is greater than the next element
-            if (abs(arr[j]) > abs(arr[j + 1])) {
-                // Swap arr[j] and arr[j+1]
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    printf("%d",arr[n-2]*arr[n-1]);
-}
 int main(){
     int n;
+    int max=-10000;
     scanf("%d",&n);
     int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    bubbleSort(arr,n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                continue;
+            }
+            if(arr[i]*arr[j]>max){
+                max=arr[i]*arr[j];
+            }
+        }
+    }
+    printf("%d",max);
+
 }
